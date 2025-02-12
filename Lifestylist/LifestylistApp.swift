fba1049a22326818295ec643fffe371eb334de1e
Jake Sanghavi <jake.sanghavi@gmail.com>
2025-02-12 13:38:04 -0500

    Initial Commit

---
+++ Lifestylist/LifestylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifestylistApp.swift
+//  Lifestylist
+//
+//  Created by Jake Sanghavi on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LifestylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
